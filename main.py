324504c4942bf3a6c384ec544df69e85be7f1ec1
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-25 13:33:16 +0200

    Wiem ze działą już

--- main.py
+++ main.py
@@ -34,3 +34,55 @@
 #pole=prostokat(a,b)
 #print(f"pole to {pole} jendostek kwadraotwych")
 
+#def srednia_predkosc(czas,dystans):
+#   return dystans/czas
+
+
+
+#to zl ejest chyba idk co miałem zrobic zopabcze na filmiku
+#f oblicz_dec():
+#    while True:
+#        category_name = input("Podaj kategorię wydatków albo zakończ wpisując 'X': ")
+#        if category_name == "X":
+#            break
+#        expenditures[category_name] = []
+#
+ #       while True:
+  #          expenditure = input(
+   #             f"Podaj wartość następnego wydatku w kategorii {category_name} albo zakończ wpisując 'X': ")
+    ###
+       #     expenditure_value = float(expenditure)
+        #    expenditures[category_name].append(expenditure_value)
+#print("Kalkulator budżetu miesięcznego")
+#expenditures = {}
+#
+#oblicz_dec()
+
+
+#total_expenditures = 0
+#for category_expenditures in expenditures.values():
+ #   total_expenditures += sum(category_expenditures)
+
+
+#expenditures_percentage = {}
+#for category_name, category_expenditures in expenditures.items():
+  #  total_category_expenditures = sum(category_expenditures)
+ #   expenditures_percentage[category_name] = total_category_expenditures * 100 / total_expenditures
+#
+#most_important_category = None
+#most_important_category_percentage = 0
+#for category, percentage in expenditures_percentage.items():
+   #
+#if most_important_category is not None:
+ #   print(f"Najwięcej wydajesz na {most_important_category} - {most_important_category_percentage:.0f}% wszystkich wydatków")
+
+#for category, percentage in expenditures_percentage.items():
+  #  print(f"Na {category} wydajesz {percentage:.0f}% miesięcznych wydatków")
+
+
+  #okej to juz kumam z filmiku nex
+
+  #Argumenty p[ozycyjne  i nazwane
+
+
+
