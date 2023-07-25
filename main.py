a04838f47945dba90837d37f0e790e1c478f1784
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-25 12:35:55 +0200

    Wiem ze działą już

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hello funckje")
+#print("hello funckje")
+#def fukncja_parametrem(i):
+#    for j in range (1,i+1):
+#        print(j)
+
+#fukncja_parametrem(20)
+
+#def best_grade_sub(list):
+#    best_sb = None
+#    best_gr = 0
+#   real_br=0
+#    for subject,grade in list.items():
+#        best_gr=max(grade)
+#        if real_br<best_gr:
+##            real_br=best_gr
+ #           best_sb=subject
+ #   return best_sb,real_br
+#dzienniczek={
+#    "matma":[2,4,5,1],
+#    "polish":[2,5,6],
+#    "eng":[2,1,3,7]
+#}#
+
+#bs,br=best_grade_sub(dzienniczek)
+#print(f"naj ocenka {br} a subject to {bs}")
+
+#zadanka
+
+#def prostokat(a,b):
+#    return a*b
+
+#a=int(input("podaj a"))
+#b=int(input("podaj b"))
+#pole=prostokat(a,b)
+#print(f"pole to {pole} jendostek kwadraotwych")
+
