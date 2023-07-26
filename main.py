56eee514c3a0bdb1811ed878226659eb3572d280
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-26 12:04:20 +0200

    Wiem ze działą już

--- main.py
+++ main.py
@@ -85,4 +85,21 @@
   #Argumenty p[ozycyjne  i nazwane
 
 
+#def avg_speed(distance, time):
+#    return distance / time
+#def run_avg_speed_calculator(vehicle_name):
+#    distance = float(input(f"Ile km pokonałeś/pokonałaś za pomocą {vehicle_name}? "))
+#    time = float(input("Ile godzin Ci to zajęło? "))
+#    average_speed = avg_speed(distance=distance, time=time)
+#    print(f"Twoja średnia prędkość przemieszczania się za pomocą {vehicle_name} to {average_speed}km/h")
+
+#run_avg_speed_calculator(vehicle_name="biegu")
+#run_avg_speed_calculator("roweru")
+#run_avg_speed_calculator("samochodu")
+
+#parametry domyślne
+
+#print("domyslny sepraratos","llolo")
+#print("nie domyslny sepraratos","llolo",sep="---")
+
 
